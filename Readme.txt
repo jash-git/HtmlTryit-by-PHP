94aba4b244116b04ed0f87869b335df271dd73bb
jash-git <jash.liao@gmail.com>
2018-11-02 11:05:55 +0800

    Create

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+﻿簡易HTML-CSS-JS線上執行器/線上測試(HtmlTryit-by-PHP)
+
+資料來源:https://github.com/shashankankar/Tryit_Editor
+
+有顏色的商業用URL:http://www.runoob.com/try/try.php?filename=tryhtml_intro
+
+GITHUB:https://github.com/jash-git/HtmlTryit-by-PHP
+
+使用規則:貼上對應的HTML碼，如果有引用的CSS/JS一定要是完整URL(不可以是相對路徑)
---
+++ try_it.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"> 
+<title>&#33756;&#40479;&#25945;&#31243;(runoob.com)</title> 
+<script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js">
+</script>
+<script>
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+</script>
+</head>
+<body>
+<p>&#22914;&#26524;&#20320;&#28857;&#25105;&#65292;&#25105;&#23601;&#20250;&#28040;&#22833;&#12290;</p>
+<p>&#32487;&#32493;&#28857;&#25105;!</p>
+<p>&#25509;&#30528;&#28857;&#25105;!</p>
+</body>
+</html>
